e1c30c72a131d4f6580366be2c6d9d5690900b38
kcvedula <vedula.k@northeastern.edu>
2024-10-24 17:47:17 -0400

    Created test stub

--- src/cs3500/threetrios/player/ThreeTriosPlayer.java
+++ src/cs3500/threetrios/player/ThreeTriosPlayer.java
@@ -1,5 +1,5 @@
 package cs3500.threetrios.player;
-
+import static org.junit.Assert.*;
 public interface ThreeTriosPlayer {
   void do_something();
 }
---
+++ test/cs3500/threetrios/test/TestUtils.java
@@ -0,0 +1,11 @@
+package cs3500.threetrios.test;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestUtils {
+
+  @Test
+  public void testAssertWorks() {
+    Assert.assertEquals(1, 1);
+  }
+}
