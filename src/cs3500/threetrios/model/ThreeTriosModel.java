64f72b213bbda9a15c72d1ab0f7192b378fe2042
CiaranFlanagan <157981950+CiaranFlanagan@users.noreply.github.com>
2024-10-28 17:19:04 -0400

    Fully implemented and partially commented  the model

--- src/cs3500/threetrios/model/ThreeTriosModel.java
+++ src/cs3500/threetrios/model/ThreeTriosModel.java
@@ -1,7 +1,12 @@
 package cs3500.threetrios.model;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class ThreeTriosModel implements GameModel{
   private GamePlayer playerRed;
@@ -32,7 +37,6 @@ public class ThreeTriosModel implements GameModel{
     this.grid = grid;
     this.gameStarted = true;
     this.gameOver = false;
-
     dealCards(cards);
   }
 
@@ -69,27 +73,62 @@ public class ThreeTriosModel implements GameModel{
     if (player != currentPlayer) {
       throw new IllegalArgumentException("It's not " + player.getName() + "'s turn");
     }
-    if (!player.getHand().contains(card)) {
+    if (!player.getHand().contains(card)) { //might have to override equals in Card
       throw new IllegalArgumentException("Player does not have this card");
     }
     BoardCell cell = grid.getCell(row, col);
     if (cell.getType() != CellType.CARD_CELL) {
       throw new IllegalArgumentException("Cannot place a card on a hole");
     }
-    CardCell cardCell = (CardCell) cell;
+    CardCell cardCell = (CardCell) cell; //need to cast here I think, might be another way
     if (cardCell.hasCard()) {
       throw new IllegalArgumentException("Cell already has a card");
     }
-    cardCell.placeCard(card, player);
+    cardCell.placeCard(card, player); //change this method name in CardCell
     player.removeCard(card);
     executeBattle(cardCell, player);
     checkGameOver();
   }
 
   private void checkGameOver() {
+    if (playerRed.getHand().isEmpty() && playerBlue.getHand().isEmpty()) {
+      gameOver = true;
+    }
   }
 
-  private void executeBattle(CardCell cardCell, GamePlayer player) {
+  private void executeBattle(CardCell placedCell, GamePlayer player) {
+    Queue<CardCell> toProcess = new LinkedList<>();
+    Set<CardCell> processed = new HashSet<>();
+    toProcess.add(placedCell);
+
+    while (!toProcess.isEmpty()) {
+      CardCell cell = toProcess.poll();
+      processed.add(cell);
+      //loops through all neighbors of the cell(should be 4 total)
+      for (Map.Entry<Direction, BoardCell> entry : cell.getNeighbors().entrySet()) {
+        Direction direction = entry.getKey();
+        BoardCell neighborCell = entry.getValue();
+        //if the neighbors a Card cell...
+        if (neighborCell.getType() == CellType.CARD_CELL) {
+          CardCell neighbor = (CardCell) neighborCell; //cast b/c neighbor cell is a BoardCell
+          //if the neighbor has a card and the owner is different get the values of each card
+          if (neighbor.hasCard() && neighbor.getOwner() != currentPlayer) {
+            int attackValue = cell.getCard().getAttackValue(direction).getNumericValue();
+            int defenseValue = neighbor.getCard().getAttackValue(direction.opposite()).getNumericValue();
+
+            //if the attack val is greater than the neighbors attack value, set the owner
+            if (attackValue > defenseValue) {
+              neighbor.setOwner(currentPlayer);
+              //if the neighbor is not already in the toProcess queue, add it so we don't process
+              // so we can process it later.
+              if (!processed.contains(neighbor)) {
+                toProcess.add(neighbor);
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   /**
@@ -99,7 +138,7 @@ public class ThreeTriosModel implements GameModel{
    */
   @Override
   public GamePlayer getCurrentPlayer() {
-    return null;
+    return currentPlayer;
   }
 
   /**
@@ -107,7 +146,13 @@ public class ThreeTriosModel implements GameModel{
    */
   @Override
   public void nextTurn() {
-
+    if (!gameStarted) {
+      throw new IllegalStateException("Game has not started yet");
+    }
+    if (gameOver) {
+      throw new IllegalStateException("Game is over");
+    }
+    currentPlayer = (currentPlayer == playerRed) ? playerBlue : playerRed;
   }
 
   /**
@@ -117,7 +162,7 @@ public class ThreeTriosModel implements GameModel{
    */
   @Override
   public boolean isGameOver() {
-    return false;
+    return gameOver;
   }
 
   /**
@@ -127,7 +172,30 @@ public class ThreeTriosModel implements GameModel{
    */
   @Override
   public GamePlayer getWinner() {
-    return null;
+    if (!gameOver) {
+      return null;
+    }
+    int redCount = 0;
+    int blueCount = 0;
+    for (BoardCell cell : grid.getAllCells()) {
+      if (cell.getType() == CellType.CARD_CELL) {
+        CardCell cardCell = (CardCell) cell;
+        if (cardCell.hasCard()) {
+          if (cardCell.getOwner() == playerRed) {
+            redCount++;
+          } else if (cardCell.getOwner() == playerBlue) {
+            blueCount++;
+          }
+        }
+      }
+    }
+    if (redCount > blueCount) {
+      return playerRed;
+    } else if (blueCount > redCount) {
+      return playerBlue;
+    } else {
+      return null; //handle with other methods
+    }
   }
 
   /**
@@ -137,6 +205,36 @@ public class ThreeTriosModel implements GameModel{
    */
   @Override
   public String renderGameState() {
-    return null;
+    StringBuilder sb = new StringBuilder();
+    sb.append("Player: ").append(currentPlayer.getName()).append("\n");
+    for (int row = 0; row < grid.getRows(); row++) {
+      for (int col = 0; col < grid.getColumns(); col++) {
+        BoardCell cell = grid.getCell(row, col);
+        if (cell.getType() == CellType.HOLE) {
+          sb.append(" ");
+        } else if (cell.getType() == CellType.CARD_CELL) {
+          CardCell cardCell = (CardCell) cell;
+          if (cardCell.hasCard()) {
+            String initial = cardCell.getOwner().getName().substring(0, 1).toUpperCase();
+            sb.append(initial);
+          } else {
+            sb.append("_");
+          }
+          sb.append(" ");
+        }
+      }
+      sb.append("\n");
+    }
+
+    sb.append("Hand:\n");
+    for (Card card : currentPlayer.getHand()) {
+      sb.append(card.getName());
+      for (Direction dir : CardinalDirection.values()) {
+        AttackValue attackValue = card.getAttackValue(dir);
+        sb.append(" ").append(attackValue.toString());
+      }
+      sb.append("\n");
+    }
+    return sb.toString();
   }
 }
