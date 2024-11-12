583c35db2507cf602589f9eec2c5325795b71f44
kcvedula <vedula.k@northeastern.edu>
2024-11-11 19:30:09 -0500

    couple changes to corner and defense. simplified

--- src/cs3500/threetrios/player/CornerStrategy.java
+++ src/cs3500/threetrios/player/CornerStrategy.java
@@ -26,22 +26,20 @@ public class CornerStrategy extends DefenseStrategy {
     super(modelSupplier);
   }
 
+  // for each corner, play each card from hand
   protected List<Move> allConsideredMoves() {
     Model model = modelSupplier.get();
     List<Move> acc = new ArrayList<>();
-    BiFunction<Integer, Integer, List<Move>> pos2Move =
-            (row, col) -> {
-              List<Move> innerAcc = new ArrayList<>();
-              IntStream.range(0, model.getCurrentCoach().getHand().size()).forEach(
-                      (handId) -> innerAcc.add(Move.of(row, col, handId)));
-              return innerAcc;
-            };
-    acc.addAll(pos2Move.apply(0, 0));
-    acc.addAll(pos2Move.apply(0, model.getGrid().readOnly2dCellArray()[0].length));
-    acc.addAll(pos2Move.apply(model.getGrid().readOnly2dCellArray().length - 1,0));
-    acc.addAll(pos2Move.apply(model.getGrid().readOnly2dCellArray().length - 1,
-                              model.getGrid().readOnly2dCellArray()[0].length - 1));
-    acc.stream().filter((move) -> {
+    int lastRow = model.getGrid().readOnly2dCellArray().length - 1;
+    int lastCol = model.getGrid().readOnly2dCellArray()[0].length - 1;
+    Consumer<Function<Integer, Move>> func = (f) ->
+            IntStream.range(0, model.getCurrentCoach().getHand().size())
+                    .forEach((handId) -> acc.add(f.apply(handId)));
+    func.accept((handId) -> Move.of(0, 0, handId));
+    func.accept((handId) -> Move.of(lastRow, 0, handId));
+    func.accept((handId) -> Move.of(lastRow, lastCol, handId));
+    func.accept((handId) -> Move.of(lastRow, lastCol, handId));
+    return acc.stream().filter((move) -> {
       try {
         move.accept(modelSupplier.get());
         return true;
@@ -49,18 +47,6 @@ public class CornerStrategy extends DefenseStrategy {
         return false;
       }
     }).collect(Collectors.toList());
-    return acc;
   }
 
-  // card cell and is empty
-  private List<CardinalDirection> getExposedSides1(int row, int col, Grid grid) {
-    return Arrays.stream(CardinalDirection.values())
-            .filter((cd) -> {
-              GridCellReadOnly cell = grid.readOnly2dCellArray()[row][col];
-              return cell.hasNeighborToThe(cd) ?
-                      (cell.getNeighborToThe(cd).canHaveCard() &&
-                              !cell.getNeighborToThe(cd).hasCard()) :
-                      false;
-            }).collect(Collectors.toList());
-  }
 }
